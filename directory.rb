485ab5b98c76cf75556956cc00dbfd99523a4e6e
zoethomas <zoethomas1540@gmail.com>
2020-10-05 09:21:06 +0100

    Added load students function

--- directory.rb
+++ directory.rb
@@ -241,10 +241,25 @@ def save_students
 
 end
 
+def load_students
+  # open the file for reading
+  file = File.open("students.csv", "r")
+
+  # call teh readlines function to iterate over each line of the file
+  file.readlines.each do |line|
+    # parallel assignment
+    name, cohort = line.chomp.split(',')
+      # put data into hash
+      $students << {name: name, cohort: cohort.to_sym}
+    end
+  file.close
+end
+
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -262,6 +277,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
