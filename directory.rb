b7093597f03f3ec12e14e0daa65c5bade3def325
zoethomas <zoethomas1540@gmail.com>
2020-10-06 14:34:35 +0100

    Fixed duplicate printing problem and added blank lines for clarity

--- directory.rb
+++ directory.rb
@@ -1,4 +1,3 @@
-$students = []
 # let's put all students into an array
 # students = [
 #   {name: "Dr. Hannibal Lecter", cohort: :november},
@@ -13,18 +12,142 @@ $students = []
 #   {name: "Joffrey Baratheon", cohort: :november},
 #   {name: "Norman Bates", cohort: :march}
 # ]
+
+$students = []
+
 def print_header
+  puts ()
   puts "The students of Villains Academy"
   puts "-------------"
+  puts ()
 end
 def print_students_list
-  $students.each do |student|
+  $students.uniq.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 def print_footer
+  puts()
   puts "Overall, we have #{$students.count} great students"
+  puts()
 end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = STDIN.gets.delete("\n")
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    add_to_hash(name, :november)
+    if $students.count == 1
+      puts "Now we have 1 student"
+    else
+      puts "Now we have #{$students.count} students"
+    end
+    # get another name from the user
+    name = STDIN.gets.delete("\n")
+  end
+  puts()
+end
+
+def load_students(filename = "students.csv")
+  puts()
+  # open the file for reading
+  file = File.open(filename, "r")
+
+  # call the readlines function to iterate over each line of the file
+  file.readlines.each do |line|
+    # parallel assignment
+    name, cohort = line.chomp.split(',')
+    # put data into hash
+    add_to_hash(name, cohort)
+  end
+  file.close
+end
+
+def add_to_hash(name, cohort)
+    $students << {name: name, cohort: cohort.to_sym}
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+
+  #iterate over the array of students
+  $students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def try_load_students
+  puts()
+  filename = ARGV.first # first argument from the command line
+  if filename.nil? # get out of the method if it isn't given
+    filename = "students.csv"
+  end
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+     puts "Loaded #{$students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
+  puts()
+end
+
+def print_menu
+  puts ()
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit"
+  puts()
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      students = input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "4"
+      load_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+      puts()
+  end
+end
+
+def interactive_menu
+  students = []
+  while true do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end
+
+try_load_students
+interactive_menu
+
+
 
 
 # EXTRA EXERCISES
@@ -207,111 +330,3 @@ end
 # students = input_more_info
 # print_more_info(students)
 # print_centered(students)
-
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
-  # get the first name
-  name = STDIN.gets.delete("\n")
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    # add the student hash to the array
-    add_to_hash(name, :november)
-    if $students.count == 1
-      puts "Now we have 1 student"
-    else
-      puts "Now we have #{$students.count} students"
-    end
-    # get another name from the user
-    name = STDIN.gets.delete("\n")
-  end
-end
-
-def load_students(filename = "students.csv")
-  # open the file for reading
-  file = File.open(filename, "r")
-
-  # call the readlines function to iterate over each line of the file
-  file.readlines.each do |line|
-    # parallel assignment
-    name, cohort = line.chomp.split(',')
-    # put data into hash
-    add_to_hash(name, cohort)
-  end
-  file.close
-end
-
-def add_to_hash(name, cohort)
-    $students << {name: name, cohort: cohort.to_sym}
-end
-
-def save_students
-  # open the file for writing
-  file = File.open("students.csv", "w")
-
-  #iterate over the array of students
-  $students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
-  file.close
-end
-
-def try_load_students
-  filename = ARGV.first # first argument from the command line
-  if filename.nil? # get out of the method if it isn't given
-    filename = "students.csv"
-  end
-  if File.exists?(filename) # if it exists
-    load_students(filename)
-     puts "Loaded #{$students.count} from #{filename}"
-  else # if it doesn't exist
-    puts "Sorry, #{filename} doesn't exist."
-    exit # quit the program
-  end
-end
-
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit"
-end
-
-def show_students
-  print_header
-  print_students_list
-  print_footer
-end
-
-def process(selection)
-  case selection
-    when "1"
-      students = input_students
-    when "2"
-      show_students
-    when "3"
-      save_students
-    when "4"
-      load_students
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again"
-  end
-end
-
-def interactive_menu
-  students = []
-  while true do
-    print_menu
-    process(STDIN.gets.chomp)
-  end
-end
-
-try_load_students
-interactive_menu
